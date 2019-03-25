d9239d8a08196eec0100ebc675dbe719f48fd6f8
bouzuya <m@bouzuya.net>
2019-03-25 19:45:51 +0900

    fix TaskResource type

--- src/Command/Task/Command/List.purs
+++ src/Command/Task/Command/List.purs
@@ -45,13 +45,28 @@ type TaskListResponse =
   , nextPageToken :: Maybe String
   }
 
--- TODO
 type TaskResource =
-  { kind :: String -- "tasks#task"
-  , id :: String
+  { completed :: Maybe String -- datetime,
+  , deleted :: Maybe Boolean
+  , due :: Maybe String -- datetime,
   , etag :: String
+  , hidden :: Maybe Boolean
+  , id :: String
+  , kind :: String -- "tasks#task"
+  , links ::
+      Maybe
+        (Array
+          { type :: String
+          , description :: String
+          , link :: String
+          })
+  , notes :: Maybe String
+  , parent :: Maybe String
+  , position :: String
+  , selfLink :: String
+  , status :: String
   , title :: String
-  -- TODO
+  , updated :: String -- datetime,
   }
 
 type TaskListParams =
